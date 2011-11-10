dd4ec555ad54856639169d39381ae0172aaa298a
Tyler Dodge <thecitystate@gmail.com>
2011-11-10 20:24:50 +0000

    fixed dumb issue

--- Escape/src/edu/ncsu/uhp/escape/engine/utilities/ColorBox.java
+++ Escape/src/edu/ncsu/uhp/escape/engine/utilities/ColorBox.java
@@ -23,7 +23,7 @@ public class ColorBox implements IRenderable {
 		this.bf = (float) (b / 255.0);
 		this.af = (float) (a / 255.0);
 		this.dimensions = dimensions;
-		this.offsets = offsets;
+		setOffsets(offsets);
 		float dimenX = dimensions.getX();
 		float dimenY = dimensions.getY();
 		float[] quadPoints = { 0.0f, dimenY, 0.0f, 
