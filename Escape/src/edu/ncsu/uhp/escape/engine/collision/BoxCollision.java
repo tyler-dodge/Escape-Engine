19ee742494d4fead4883e6214932f2baba9152c4
Tyler Dodge <thecitystate@gmail.com>
2011-11-10 18:46:00 +0000

    Added collision box rendering

--- Escape/src/edu/ncsu/uhp/escape/engine/collision/BoxCollision.java
+++ Escape/src/edu/ncsu/uhp/escape/engine/collision/BoxCollision.java
@@ -1,5 +1,10 @@
 package edu.ncsu.uhp.escape.engine.collision;
 
+import javax.microedition.khronos.opengles.GL10;
+
+import edu.ncsu.uhp.escape.engine.utilities.ColorSource;
+import edu.ncsu.uhp.escape.engine.utilities.IRenderable;
+import edu.ncsu.uhp.escape.engine.utilities.RenderSource;
 import edu.ncsu.uhp.escape.engine.utilities.math.Point;
 
 /**
@@ -10,6 +15,7 @@ import edu.ncsu.uhp.escape.engine.utilities.math.Point;
  */
 public class BoxCollision extends MultiPhaseCollision {
 
+	private RenderSource source;
 	/**
 	 * Constructs a Collision Box from the Points dimension and offsets.
 	 * 
@@ -19,5 +25,10 @@ public class BoxCollision extends MultiPhaseCollision {
 	public BoxCollision(Point dimension, Point offsets) {
 		super(new OB_BroadPhase(dimension, offsets), new AABB_NarrowPhase(
 				dimension, offsets));
+		this.source=new ColorSource(this.hashCode(), 255,0,255, 255, dimension, offsets);
+	}
+	public IRenderable getRenderable(GL10 gl)
+	{
+		return source.getData(gl);
 	}
 }
