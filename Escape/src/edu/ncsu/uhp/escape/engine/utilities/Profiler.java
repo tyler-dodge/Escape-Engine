1ed81b6e0f9eb2d0787178405ed8948157160023
Tyler Dodge <thecitystate@gmail.com>
2011-11-10 19:48:11 +0000

    made it so collision boxes are shinier. And added a final to profiler to stop it from outputting

--- Escape/src/edu/ncsu/uhp/escape/engine/utilities/Profiler.java
+++ Escape/src/edu/ncsu/uhp/escape/engine/utilities/Profiler.java
@@ -25,14 +25,17 @@ public class Profiler {
 		}
 	}
 
+	private static final boolean DO_PROFILE = false;
+
 	private Profiler() {
 		sections = new Stack<Profiler.ProfileSection>();
-		threadId=threadCount++;
+		threadId = threadCount++;
 	}
-	private static int threadCount=0;
-	private int threadId=0;
+
+	private static int threadCount = 0;
+	private int threadId = 0;
 	private Stack<ProfileSection> sections;
-	private static Dictionary<Thread, Profiler> instance=new Hashtable<Thread, Profiler>();
+	private static Dictionary<Thread, Profiler> instance = new Hashtable<Thread, Profiler>();
 	private int frameNum = 0;
 
 	public static Profiler getInstance() {
@@ -49,15 +52,21 @@ public class Profiler {
 	}
 
 	public void startSection(String name) {
-		ProfileSection section = new ProfileSection(name);
-		sections.push(section);
-		Log.d("Frame " + frameNum, "T: "+threadId+" S:" + sections.size()
-				+ " Start Empty \"Starting " + name + "\"");
+		if (DO_PROFILE) {
+			ProfileSection section = new ProfileSection(name);
+			sections.push(section);
+			Log.d("Frame " + frameNum,
+					"T: " + threadId + " S:" + sections.size()
+							+ " Start Empty \"Starting " + name + "\"");
+		}
 	}
 
 	public void endSection() {
-		int size = sections.size();
-		ProfileSection section = sections.pop();
-		Log.d("Frame " + frameNum, "T: "+threadId+" S:" + size + " End " + section.toString());
+		if (DO_PROFILE) {
+			int size = sections.size();
+			ProfileSection section = sections.pop();
+			Log.d("Frame " + frameNum, "T: " + threadId + " S:" + size
+					+ " End " + section.toString());
+		}
 	}
 }
