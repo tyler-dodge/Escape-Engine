9dd13da33635eb2388148a7f915ba08c3c4afaf9
Tyler Dodge <thecitystate@gmail.com>
2011-11-28 20:48:06 +0000

    reintegrated collisions to main for in field testing

--- Escape/src/edu/ncsu/uhp/escape/engine/collision/IBroadCollision.java
+++ Escape/src/edu/ncsu/uhp/escape/engine/collision/IBroadCollision.java
@@ -3,7 +3,7 @@ package edu.ncsu.uhp.escape.engine.collision;
 import edu.ncsu.uhp.escape.engine.utilities.math.Point;
 import edu.ncsu.uhp.escape.engine.utilities.*;
 
-public interface IBroadCollision {
+public interface IBroadCollision extends ICollision {
 	public abstract boolean doesCollideBroad(Point thisPosition,
 			IRotation thisRotation, ICollision checkCollide,
 			Point checkPosition, IRotation checkRotation);
