a738f065ddfb9f31ba6f590f115c9650413ee027
Tyler Dodge <thecitystate@gmail.com>
2011-12-12 09:38:36 +0000

    Added a setText method to TextGraphic

--- Escape/src/edu/ncsu/uhp/escape/engine/utilities/TextGraphic.java
+++ Escape/src/edu/ncsu/uhp/escape/engine/utilities/TextGraphic.java
@@ -3,14 +3,23 @@ package edu.ncsu.uhp.escape.engine.utilities;
 import edu.ncsu.uhp.escape.engine.utilities.math.Point;
 
 public class TextGraphic extends Graphic {
+	private float height;
+	private int r, g, b, a;
+
 	public TextGraphic(Point position, String text, float height) {
-		this(position, text, height,255,0,0,0);
+		this(position, text, height, 255, 0, 0, 0);
 
 	}
+
 	public TextGraphic(Point position, String text, float height, int r, int g,
 			int b, int a) {
 		super(position, ZAxisRotation.getIdentity(), new TextRenderSource(0,
 				text, height, new Point(0, 0, 0), r, g, b, a));
 
+	}
+
+	public void setText(String text) {
+		this.setRenderSource(new TextRenderSource(0, text, height, new Point(0,
+				0, 0), r, g, b, a));
 	}
 }
